ff49e996e034b6b592b059aedbc30c48ddb35b54
mina-gh <minamail@gmail.com>
2018-08-24 11:24:40 -0400

    added the Provider component to the client index file

--- client/index.js
+++ client/index.js
@@ -7,11 +7,15 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 import { Main } from './components';
 
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
     <Main />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById('app')
 );
