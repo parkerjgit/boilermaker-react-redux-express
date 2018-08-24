1ad0eee5bedfd4b1ba439f9f5378864a77944fc1
mina-gh <minamail@gmail.com>
2018-08-24 13:03:54 -0400

    worked on the store for writing and getting a message

--- client/store.js
+++ client/store.js
@@ -3,6 +3,8 @@ import thunkMiddleware from 'redux-thunk';
 import axios from 'axios';
 
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const WRITE_MESSAGE = 'WRITE_MESSAGE'; // get the input text from the UI and post it in db.
+const GOT_NEW_MESSAGE_FROM_SERVER = 'GOT_NEW_MESSAGE_FROM_SERVER';
 
 export const gotMessagesFromServer = messages => ({
   type: GOT_MESSAGES_FROM_SERVER,
@@ -11,19 +13,49 @@ export const gotMessagesFromServer = messages => ({
 
 export const getMessagesFromServer = () => (
   async (dispatch) => {
-    const { data } = await axios.get('/api/messages');
-    dispatch(gotMessagesFromServer(data));
+    try {
+      const { data } = await axios.get('/api/messages');
+      dispatch(gotMessagesFromServer(data));
+    } catch (err){
+      console.error(err.stack);
+    }
+  }
+)
+
+export const writeMessage = inputText => ({
+  type: WRITE_MESSAGE,
+  newMessageEntry: inputText
+})
+
+export const gotMessageFromServer = (newMessage) => ({
+  type: GOT_NEW_MESSAGE_FROM_SERVER,
+  newMessage
+})
+
+export const getMessageFromServer = (newMessageEntry) => (
+  async (dispatch) => {
+   try{
+    const {data} = await axios.post('/api/messages', newMessageEntry);
+    dispatch(gotMessageFromServer(data));
+   } catch(err){
+     console.error(err.stack);
+   }
   }
 )
 
 const initialState = {
   messages: [],
+  newMessageEntry: '', // inputText from the UI
 }
 
 const reducer = (prevState = initialState, action) => {
   switch (action.type) {
     case GOT_MESSAGES_FROM_SERVER:
       return {...prevState, messages: action.messages};
+    case GOT_NEW_MESSAGE_FROM_SERVER:
+      return {...prevState, messages: [...prevState.messages, action.newMessage]};
+    case WRITE_MESSAGE:
+      return {...prevState, newMessageEntry: action.newMessageEntry}
     default:
       return prevState;
   }
