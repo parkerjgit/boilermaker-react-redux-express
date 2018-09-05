d57ebcfb1e0d855bb4a6a2e66e60892657c1d7f9
Joshua Parker <joshuaparker@protonmail.com>
2018-09-05 18:12:58 -0400

    setup sessions

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const db = require('./db')
+
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -42,7 +44,30 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || 'Internal server error.')
 })
 
-const db = require('./db')
+// sessions ---
+const session = require('express-session');
+
+// configure and create our database store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+
+// sync so that our session table gets created
+dbStore.sync();
+
+// plug the store into our session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+  store: dbStore,
+  resave: false,
+  saveUninitialized: false
+}));
+// Then, on our deployment server, we can set an environment variable called SESSION_SECRET with our real secret!
+// Our secrets would still be exposed if an attacker got access to our server (but if that happens, we're pretty much screwed anyway). So as long as we protect access to our deployment server, we should be fine.
+
+
+// start it up -------------------
+
+
 const PORT = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
 
 db.sync()  // sync our database
